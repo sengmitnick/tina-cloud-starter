7e1f73c126cc4360dfeb3a18c7af37b578d3f63a
SengMitnick <sengmitnick@163.com>
2023-08-30 21:50:14 +0800

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,12 +4,7 @@ export default class CustomDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"
-          />
-        </Head>
+        <Head></Head>
         <body>
           <Main />
           <NextScript />
