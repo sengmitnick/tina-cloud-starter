6ff3f45e15cd070057dc64d60b0fa2e643edb387
SengMitnick <sengmitnick@163.com>
2023-08-30 21:39:10 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,7 @@
-import { AppProps } from 'next/app';
-import Head from 'next/head';
 import '../styles.css';
 
-const App = ({ Component, pageProps }: AppProps) => {
-  return (
-    <>
-      <Head>
-        <meta
-          name="description"
-          content="我是程序员大胜， 一个前端开发工程师。"
-        />
-        <meta name="keywords" content="技术随笔, IDE, 编程, AI, AIGC, 大胜" />
-        <title>大胜的技术随笔</title>
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+const App = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
 };
 
 export default App;
